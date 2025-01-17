70a7531076c2e9502148280a1e88bb5b943444b3
LilaniRanjan <ranjanlilani@gmail.com>
2025-01-17 18:14:02 +0530

    fetch only last record function done

--- DOTMusiccrudApp/urls.py
+++ DOTMusiccrudApp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('artists/', views.get_artists, name='get_artists'),
-    path('albums/', views.get_albums, name='get_albums'),
-    path('popular-radio/', views.get_popular_radio, name='get_popular_radio'),
-    path('today-in-music/', views.get_today_in_music, name='get_today_in_music'),
+    path('artists/', views.get_lastfive_artists, name='get_artists'),
+    path('albums/', views.get_lastfive_albums, name='get_albums'),
+    path('popular-radio/', views.get_lastfive_popular_radio, name='get_popular_radio'),
+    path('today-in-music/', views.get_lastfive_today_in_music, name='get_today_in_music'),
 ]
--- DOTMusiccrudApp/views.py
+++ DOTMusiccrudApp/views.py
@@ -1,6 +1,24 @@
 from django.http import JsonResponse
 from .models import Artist, Album, PopularRadio, TodayInMusic
 
+def get_lastfive_artists(request):
+    artists = list(Artist.objects.order_by('-id')[:5].values())  # Fetch last 5 records
+    return JsonResponse({'artists': artists}, safe=False)
+
+def get_lastfive_albums(request):
+    albums = list(Album.objects.order_by('-id')[:5].values())  # Fetch last 5 records
+    return JsonResponse({'albums': albums}, safe=False)
+
+def get_lastfive_popular_radio(request):
+    radios = list(PopularRadio.objects.order_by('-id')[:5].values())  # Fetch last 5 records
+    return JsonResponse({'popular_radio': radios}, safe=False)
+
+def get_lastfive_today_in_music(request):
+    today_in_music = list(TodayInMusic.objects.order_by('-id')[:5].values())  # Fetch last 5 records
+    return JsonResponse({'today_in_music': today_in_music}, safe=False)
+
+# --------------------------------------------------------------------------------------
+
 def get_artists(request):
     artists = list(Artist.objects.values())
     return JsonResponse({'artists': artists}, safe=False)
@@ -16,3 +34,4 @@ def get_popular_radio(request):
 def get_today_in_music(request):
     today_in_music = list(TodayInMusic.objects.values())
     return JsonResponse({'today_in_music': today_in_music}, safe=False)
+
