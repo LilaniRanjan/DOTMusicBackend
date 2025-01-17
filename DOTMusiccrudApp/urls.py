cd5919ef1013f29035fccd0184780e2d61e08178
LilaniRanjan <ranjanlilani@gmail.com>
2025-01-17 18:51:50 +0530

    Fetch all the artist part done

--- DOTMusiccrudApp/urls.py
+++ DOTMusiccrudApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('albums/', views.get_lastfive_albums, name='get_albums'),
     path('popular-radio/', views.get_lastfive_popular_radio, name='get_popular_radio'),
     path('today-in-music/', views.get_lastfive_today_in_music, name='get_today_in_music'),
+    path('full-artists/', views.get_artists, name='get_full_artists'),
 ]
